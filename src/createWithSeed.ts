f12dedbe7cdd943f1a20fc0933befb61b115b985
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-05-28 15:33:59 -0400

    fix(createWithSeed): fix upper limits

--- src/createWithSeed.ts
+++ src/createWithSeed.ts
@@ -12,9 +12,9 @@ export function createWithSeed(seed: number): UuidGenerator {
   const timeLow = getRandomInteger(seed, 0, LIMIT_UI32 - 1)
   const timeMid = getRandomInteger(timeLow, 0, LIMIT_UI16 - 1)
   const timeHi = 0x4000 | getRandomInteger(timeMid, 0, LIMIT_UI12 - 1)
-  const clockSeqHi = 0x80 | getRandomInteger(timeHi, 0, LIMIT_UI06)
-  const clockSeqLow = getRandomInteger(clockSeqHi, 0, LIMIT_UI08)
-  const node = getRandomInteger(clockSeqLow, 0, LIMIT_UI48)
+  const clockSeqHi = 0x80 | getRandomInteger(timeHi, 0, LIMIT_UI06 - 1)
+  const clockSeqLow = getRandomInteger(clockSeqHi, 0, LIMIT_UI08 - 1)
+  const node = getRandomInteger(clockSeqLow, 0, LIMIT_UI48 - 1)
 
   return new UuidGenerator(timeLow, timeMid, timeHi, clockSeqHi, clockSeqLow, node)
 }
